58df6339796997dda73565b275600d7f7f118c11
bmlancien <bryan.lancien@rl-institut.de>
2021-04-19 12:10:39 +0200

    use AOS library for scroll animation

--- js/project.js
+++ js/project.js
@@ -4,23 +4,8 @@ var lazyLoadInstance = new LazyLoad({
   // Your custom settings go here
 });
 
-// Animate sections
-const allSections = document.querySelectorAll('.js-section');
-
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.add('js-section--animate');
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
+// Animations on scroll
+AOS.init({
+  duration: 1000,
+  disable: "mobile"
 });
