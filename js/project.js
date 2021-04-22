12f893f130c1bc3e266f5381c1de4be194d8fa2d
bmlancien <bryan.lancien@rl-institut.de>
2021-04-22 15:07:08 +0200

    highlight nav items on scroll

--- js/project.js
+++ js/project.js
@@ -9,3 +9,36 @@ AOS.init({
   duration: 1000,
   disable: "mobile"
 });
+
+// Highlight navigation item on scroll
+// https://codepen.io/mishunov/pen/opeRdL
+const sections = document.querySelectorAll('section.section');
+const config = {
+  rootMargin: '-50px 0px -55%'
+};
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      intersectionHandler(entry); 
+    }
+  });
+}, config);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+function intersectionHandler(entry) {
+  const id = entry.target.id;
+  const currentlyActive = document.querySelector('nav li a.active');
+  const shouldBeActive = document.querySelector('nav li a[data-ref=' + id + ']');
+
+  if (currentlyActive) {
+    currentlyActive.classList.remove('active');
+  }
+  if (shouldBeActive) {
+    shouldBeActive.classList.add('active');
+  }
+}
